ff2a1d568ef3f99bbc6a40e051e2c5888443db59
blseich <blseich@gmail.com>
2017-07-11 23:05:31 -0400

    fix text issue on mobile
    -TODO: still an issue with iPhone5 in landscape mode

--- src/js/Home.js
+++ src/js/Home.js
@@ -27,14 +27,14 @@ class Home extends Component {
                 </div>
                 <div className="foreground row">
                     <div className={"center-piece col l4 m6 s10 blue darken-2 offset-l4 offset-m3 offset-s1"}>
-                        <div className="auto-height">
+                        <div className="half-height">
                             <div className="row no-margin">
-                                <div className="col l12 m6 s12">
+                                <div className="col l12 m6 s12 auto-height">
                                     <h4 className="col offset-s1 s10">Abby Querry</h4>
                                     <br />
                                     <h6 className="col offset-s2 s8 italic">Class of 2019</h6>
                                 </div>
-                                <div className="col l12 m6 s12 half-height">
+                                <div className="col l12 m6 s12 portrait-wrapper">
                                     <img className="full-height portrait" src={portrait}/>
                                 </div>
                             </div>
